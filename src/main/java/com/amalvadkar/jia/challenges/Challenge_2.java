9000d4bb7b757340d58b02b4d8435ee556280f1c
Abhishek Malvadkar <abhi@dev.com>
2024-10-28 12:48:29 +0530

    Challenge_2 test pass with BigDecimal input and String as output

--- src/main/java/com/amalvadkar/jia/challenges/Challenge_2.java
+++ src/main/java/com/amalvadkar/jia/challenges/Challenge_2.java
@@ -1,12 +1,14 @@
 package com.amalvadkar.jia.challenges;
 
+import java.math.BigDecimal;
+
 public class Challenge_2 {
 
     /*
         Take salary and increment amount
         and return new salary
      */
-    public static double run(double salary , double incrementAmount){
-        return salary + incrementAmount;
+    public static String run(BigDecimal salary , BigDecimal incrementAmount){
+        return salary.add(incrementAmount).toString();
     }
 }
--- src/test/java/com/amalvadkar/jia/challenges/Challenge_2Test.java
+++ src/test/java/com/amalvadkar/jia/challenges/Challenge_2Test.java
@@ -2,15 +2,17 @@ package com.amalvadkar.jia.challenges;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Challenge_2Test {
 
     @Test
     void run_test() {
-        double salary = 500.23;
-        double incrementAmount = 130.45;
+        BigDecimal salary = new BigDecimal("500.23");
+        BigDecimal incrementAmount = new BigDecimal("130.45");
         assertThat(Challenge_2.run(salary, incrementAmount))
-                .isEqualTo(630.68);
+                .isEqualTo("630.68");
     }
 }
