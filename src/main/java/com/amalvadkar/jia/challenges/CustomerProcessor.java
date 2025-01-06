ed083a3f97f4f08ed5286503a2e687e2588bda31
Abhishek Malvadkar <abhi@dev.com>
2025-01-06 11:43:09 +0530

    CustomerProcessor refactoring

--- src/main/java/com/amalvadkar/jia/challenges/CustomerProcessor.java
+++ src/main/java/com/amalvadkar/jia/challenges/CustomerProcessor.java
@@ -3,30 +3,34 @@ package com.amalvadkar.jia.challenges;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerProcessor {
 
+    public static final String CUSTOMER_DATA_DELIMETER = "~";
+
     public static List<Customer> extractCustomers(String customers){
-        List<String> customerList = customers.lines()
+        return customers.lines()
+                .map(CustomerProcessor::splitWithDelimeter)
+                .map(CustomerProcessor::parseCustomer)
                 .toList();
-        List<Customer> finalCustomers = new ArrayList<>();
-        for (String customerLine : customerList) {
-            String[] customerDetails = customerLine.split("~");
-            var customer = new Customer(
-                    Long.valueOf(String.valueOf(customerDetails[0])),
-                    customerDetails[1],
-                    customerDetails[2],
-                    customerDetails[3],
-                    customerDetails[4],
-                    customerDetails[5],
-                    LocalDate.parse(customerDetails[6], DateTimeFormatter.ofPattern("dd-MM-yyyy")),
-                    new BigDecimal(String.valueOf(customerDetails[7]))
-            );
-            finalCustomers.add(customer);
-        }
-        return finalCustomers;
+    }
+
+    private static String[] splitWithDelimeter(String customerLine) {
+        return customerLine.split(CUSTOMER_DATA_DELIMETER);
+    }
+
+    private static Customer parseCustomer(String[] customerDetails) {
+        return new Customer(
+                Long.valueOf(String.valueOf(customerDetails[0])),
+                customerDetails[1],
+                customerDetails[2],
+                customerDetails[3],
+                customerDetails[4],
+                customerDetails[5],
+                LocalDate.parse(customerDetails[6], DateTimeFormatter.ofPattern("dd-MM-yyyy")),
+                new BigDecimal(String.valueOf(customerDetails[7]))
+        );
     }
 
 }
