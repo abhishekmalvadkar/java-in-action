6e32c1b868f2dfa2980ee4905ed340e497acc617
Abhishek Malvadkar <abhi@dev.com>
2025-01-07 12:38:25 +0530

    StringProcessor word count refactoring

--- src/main/java/com/amalvadkar/jia/challenges/StringProcessor.java
+++ src/main/java/com/amalvadkar/jia/challenges/StringProcessor.java
@@ -1,11 +1,18 @@
 package com.amalvadkar.jia.challenges;
 
+import java.util.regex.Pattern;
+
 public interface StringProcessor {
 
     String ANY_NUMBER_OF_SPACE_DELIMETER = "\\s+";
 
     static int countWord(String content){
-        return content.split(ANY_NUMBER_OF_SPACE_DELIMETER).length;
+        // Approach 1 - screaming that we need regex
+       return Pattern.compile(ANY_NUMBER_OF_SPACE_DELIMETER)
+                .split(content).length;
+
+        // Approach 2 - Not screaming that we need regex so developer can do mistake by passing just literal which can be understandable in different wat (e.g .(dot means any character))
+        // return content.split(ANY_NUMBER_OF_SPACE_DELIMETER).length;
     }
 
 }
