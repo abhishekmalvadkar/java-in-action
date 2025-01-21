70a74367c72fe50b7b9300864fc1f3f397c7fd9f
Abhishek Malvadkar <abhi@dev.com>
2025-01-21 19:54:55 +0530

    TimeConverterTest Parameterized test with method source technique

--- src/test/java/com/amalvadkar/jia/exercise/TimeConverterTest.java
+++ src/test/java/com/amalvadkar/jia/exercise/TimeConverterTest.java
@@ -1,19 +1,35 @@
 package com.amalvadkar.jia.exercise;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class TimeConverterTest {
 
-    @Test
-    void convertHoursToMinutes() {
-        assertThat(TimeConverter.convertHoursToMinutes(2))
-                .isEqualTo(120);
-        assertThat(TimeConverter.convertHoursToMinutes(5))
-                .isEqualTo(300);
-        assertThat(TimeConverter.convertHoursToMinutes(-4))
-                .isEqualTo(-1);
+    public static Stream<Arguments> convertHoursToMinutesSource() {
+        return Stream.of(
+                arguments(2 , 120),
+                arguments(5 , 300),
+                arguments(-4 , -1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "convertHoursToMinutesSource")
+    void convertHoursToMinutes(int hours , int minutes) {
+//        assertThat(TimeConverter.convertHoursToMinutes(2))
+//                .isEqualTo(120);
+//        assertThat(TimeConverter.convertHoursToMinutes(5))
+//                .isEqualTo(300);
+//        assertThat(TimeConverter.convertHoursToMinutes(-4))
+//                .isEqualTo(-1);
+        assertThat(TimeConverter.convertHoursToMinutes(hours)).isEqualTo(minutes);
     }
 
     @Test
