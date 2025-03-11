ee37a95ece36e324754679c30089db25401e501b
Abhishek Malvadkar <abhi@dev.com>
2025-03-11 15:00:57 +0530

    Mask card number refactoring

--- src/main/java/com/amalvadkar/jia/banking/CardNumberUtils.java
+++ src/main/java/com/amalvadkar/jia/banking/CardNumberUtils.java
@@ -6,6 +6,8 @@ import java.util.logging.Logger;
 public class CardNumberUtils {
 
     private static final Logger logger = Logger.getLogger(CardNumberUtils.class.getName());
+    private static final String NUMBER_ONLY_REGEX = "[0-9]";
+    public static final String MASK_SYMBOL = "*";
 
     /*
         "1234-5678-9876-5432"
@@ -13,7 +15,7 @@ public class CardNumberUtils {
      */
     public static String maskCardNumber(String cardNumber){
         String maskedCardNumbers = cardNumber.substring(0, 15)
-                .replaceAll("[0-9]", "*");
+                .replaceAll(NUMBER_ONLY_REGEX, MASK_SYMBOL);
         logger.log(Level.INFO, "maskedCardNumbers = {0}", maskedCardNumbers);
 
         String endedVisibleFourNumbers = cardNumber.substring(15, 19);
